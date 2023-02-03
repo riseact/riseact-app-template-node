0bb68b11a632a9146e1083d3a7b000e75f227801
fabri <fabrizio.arzeni@metadonors.it>
2023-02-03 18:10:17 +0100

    base dev and production server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,52 @@
-import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import dotenv from "dotenv"
+import express, { Express } from "express"
+import { readFileSync } from "fs"
+import { join } from "path"
+import serveStatic from 'serve-static'
+import { createServer as createViteServer } from 'vite'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = 3001;
+const isProd = process.env.NODE_ENV === "production"
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Riseact');
-});
+const STATIC_PATH =
+  isProd
+    ? `${process.cwd()}/frontend/dist`
+    : `${process.cwd()}/frontend/`
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+async function createServer() {
+  const app: Express = express()
+  const PORT = parseInt(
+    process.env.BACKEND_PORT || process.env.PORT || "3001",
+    10
+  )
+
+  if(isProd) {
+    app.use(express.json())
+    app.use(serveStatic(STATIC_PATH, { index: false }))
+    app.use("/*", async (_req, res, _next) => {
+      return res
+        .status(200)
+        .set("Content-Type", "text/html")
+        .send(readFileSync(join(STATIC_PATH, "index.html")))
+    })
+
+  } else {
+    const vite = await createViteServer({
+      server: {
+        middlewareMode: true,
+      },
+      root: `${process.cwd()}/frontend`,
+      configFile: `${process.cwd()}/frontend/vite.config.ts`,
+    })
+  
+    app.use(vite.middlewares)
+  }
+
+  app.listen(PORT, () => {
+    console.log(join(STATIC_PATH, "index.html"))
+    console.log(`⚡️[app]: Server is running at http://localhost:${PORT}`)
+  })
+}
+
+createServer()
