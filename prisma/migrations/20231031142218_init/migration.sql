b47781b6dc52b1c0e5249803b006b7529e38d260
Jonathan Mataloni <jo.mataloni@gmail.com>
2023-10-31 15:24:25 +0100

    squash migrations

--- prisma/migrations/20231031142218_init/migration.sql
+++ prisma/migrations/20231031142218_init/migration.sql
@@ -3,5 +3,7 @@ CREATE TABLE "OrganizationCredentials" (
     "organizationId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "accessToken" TEXT NOT NULL,
     "refreshToken" TEXT NOT NULL,
-    "clientToken" TEXT NOT NULL
+    "clientToken" TEXT NOT NULL,
+    "expiresDateUTC" DATETIME NOT NULL,
+    "expiresInSeconds" INTEGER NOT NULL
 );
